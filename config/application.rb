88938370af74937eb1d014fdc4572cd0e682f3cf
yukimura1227 <takamura1227@gmail.com>
2019-01-03 15:28:34 +0900

    chore: set defult locale ja

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module Librarian
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    I18n.config.available_locales = :ja
+    I18n.default_locale = :ja
   end
 end
