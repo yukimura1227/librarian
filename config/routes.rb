3cc5cfd3c9391165abed26d8aa73130168ace5bd
yukimura1227 <takamura1227@gmail.com>
2019-01-03 12:06:42 +0900

    refact: rails d scaffold Blog

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
     delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
   resources :comments
-  resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/factories/blogs.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :blog do
-    title { "MyString" }
-    content { "MyText" }
-  end
-end
