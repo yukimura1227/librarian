514b59182584d4a7a5d86f2cdcfa2e79d95f4ce8
yukimura1227 <takamura1227@gmail.com>
2019-01-04 12:36:53 +0900

    feat: require login exclude top

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
+# for base controller
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
 end
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,6 @@
 # for top controller
 class TopController < ApplicationController
+  skip_before_action :authenticate_user!
   def index
   end
 end
