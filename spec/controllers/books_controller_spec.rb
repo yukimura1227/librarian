5d0105ca71f813d60085350d254592abd84621e3
yukimura1227 <takamura1227@gmail.com>
2019-01-05 23:45:13 +0900

    test: fix spec

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -57,40 +57,11 @@ RSpec.describe BooksController, type: :controller do
     end
   end
 
-  describe "GET #new" do
-    it "returns a success response" do
-      get :new, params: {}, session: valid_session
-      expect(response).to be_successful
-    end
-  end
-
   describe "GET #edit" do
     it "returns a success response" do
       book = Book.create! valid_attributes
       get :edit, params: {id: book.to_param}, session: valid_session
       expect(response).to be_successful
-    end
-  end
-
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Book" do
-        expect {
-          post :create, params: {book: valid_attributes}, session: valid_session
-        }.to change(Book, :count).by(1)
-      end
-
-      it "redirects to the created book" do
-        post :create, params: {book: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(Book.last)
-      end
-    end
-
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'new' template)" do
-        post :create, params: {book: invalid_attributes}, session: valid_session
-        expect(response).to be_successful
-      end
     end
   end
 
