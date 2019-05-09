930f336badb1e51ad7a261316673949eb2c82607
yukimura1227 <takamura1227@gmail.com>
2019-05-09 16:55:31 +0900

    feat: implements Book#rentaled

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,9 @@ class Book < ApplicationRecord
   belongs_to :user, optional: true
   has_one :last_rental_operation, -> { order(id: :desc) }, class_name: 'Operation::Rental'
 
+  scope :rentaled, -> {
+    where.not(user_id: nil)
+  }
 
   def rentaled?
     user.present?
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
+  describe 'Scopes' do
+    describe '#rentaled' do
+      subject { described_class.rentaled }
+
+      let!(:rentaled_book) { create(:book, :rentaled) }
+      let!(:not_rentaled_book) { create(:book) }
+
+      it { is_expected.to include rentaled_book}
+      it { is_expected.not_to include not_rentaled_book}
+    end
+  end
+
   describe 'rentaled?' do
     subject { book.rentaled? }
 
