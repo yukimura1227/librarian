f65bbfde2d7ebc88e67a673a6394664b56cc3be7
yukimura1227 <takamura1227@gmail.com>
2019-05-09 16:50:16 +0900

    test: add test for Book#rentaled_by?(user)

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -16,4 +16,21 @@ RSpec.describe Book, type: :model do
       it { is_expected.to eq true }
     end
   end
+  describe 'rentaled_by?' do
+    subject { book.rentaled_by?(user) }
+
+    context 'when book.user is same to argument' do
+      let(:book) { create(:book, :rentaled) }
+      let(:user) { book.user }
+
+      it { is_expected.to eq true }
+    end
+
+    context 'when user_id is not null' do
+      let(:book) { create(:book, :rentaled) }
+      let(:user) { create(:user) }
+
+      it { is_expected.to eq false }
+    end
+  end
 end
