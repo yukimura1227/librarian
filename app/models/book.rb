74ad3f1ec85bb4eaebe4a67ce7bb8b691544731f
yukimura1227 <takamura1227@gmail.com>
2019-05-09 18:56:22 +0900

    feat: implements Book.rented_for_a_long_time?

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 # model for books
 class Book < ApplicationRecord
+  RENTAL_LIMIT_DAYS = 30
   belongs_to :order
   belongs_to :user, optional: true
   has_one :last_rental_operation, -> { order(id: :desc) }, class_name: 'Operation::Rental'
@@ -7,6 +8,12 @@ class Book < ApplicationRecord
   scope :rentaled, -> {
     where.not(user_id: nil)
   }
+
+  def rented_for_a_long_time?
+    return false unless rentaled?
+
+    last_rental_operation.created_at <= RENTAL_LIMIT_DAYS.days.ago
+  end
 
   def rentaled?
     user.present?
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -8,8 +8,33 @@ RSpec.describe Book, type: :model do
       let!(:rentaled_book) { create(:book, :rentaled) }
       let!(:not_rentaled_book) { create(:book) }
 
-      it { is_expected.to include rentaled_book}
-      it { is_expected.not_to include not_rentaled_book}
+      it { is_expected.to include rentaled_book }
+      it { is_expected.not_to include not_rentaled_book }
+    end
+  end
+
+  describe 'rented_for_a_long_time?' do
+    subject { book.rented_for_a_long_time? }
+
+    context 'when book is rentaled' do
+      let(:book) { create(:book) }
+      let!(:older_rental_operation) { create(:rental_operation, book: book, created_at: (Book::RENTAL_LIMIT_DAYS).days.ago) }
+
+      it { is_expected.to eq false }
+    end
+
+    context 'when book is not rentaled' do
+      let(:book) { create(:book, :rentaled) }
+      context 'when last rental operation is recent' do
+        let!(:recent_rental_operation) { create(:rental_operation, book: book, created_at: (Book::RENTAL_LIMIT_DAYS - 1).days.ago) }
+
+        it { is_expected.to eq false }
+      end
+      context 'when last rental operation is recent' do
+        let!(:older_rental_operation) { create(:rental_operation, book: book, created_at: Book::RENTAL_LIMIT_DAYS.days.ago) }
+
+        it { is_expected.to eq true }
+      end
     end
   end
 
@@ -28,6 +53,7 @@ RSpec.describe Book, type: :model do
       it { is_expected.to eq true }
     end
   end
+
   describe 'rentaled_by?' do
     subject { book.rentaled_by?(user) }
 
