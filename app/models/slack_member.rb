5454b212b16700f1e22720f1b412fad0c92ac654
yukimura1227 <takamura1227@gmail.com>
2019-09-23 12:17:19 +0900

    feat: implements SlackMember.refresh_slack_members

--- app/models/slack_member.rb
+++ app/models/slack_member.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 class SlackMember < ApplicationRecord
+  def self.refresh_slack_members
+    return unless ENV['SLACK_ACCESS_TOKEN']
+
+    Slack.users_list['members'].each do |u|
+      SlackMember.find_or_create_by(
+        slack_id: u['id'],
+        slack_user_name: u['name']
+      )
+    end
+  end
 end
