da14f1adb9165fd636a24c179933cb1540cf9e12
yukimura1227 <takamura1227@gmail.com>
2019-09-24 17:00:11 +0900

    feat: slackと紐付いていない人を通知する機能

--- app/models/concerns/utility/slack.rb
+++ app/models/concerns/utility/slack.rb
@@ -4,11 +4,18 @@ module Utility
   # for posting slack
   module Slack
     extend ActiveSupport::Concern
-    def notify_slack(message)
-      return if Rails.application.config.slack_webhook_url.blank?
+    # class method
+    module ClassMethods
+      def notify_slack(message)
+        return if Rails.application.config.slack_webhook_url.blank?
 
-      notifier = Slack::Notifier.new(Rails.application.config.slack_webhook_url)
-      notifier.ping(message, parse: 'full')
+        notifier = ::Slack::Notifier.new(Rails.application.config.slack_webhook_url)
+        notifier.ping(message, parse: 'full')
+      end
+    end
+
+    def notify_slack(message)
+      self.class.notify_slack(message)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 # login user
 class User < ActiveRecord::Base
+  include Utility::Slack
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :omniauthable, omniauth_providers: %i(google_oauth2)
@@ -11,6 +12,14 @@ class User < ActiveRecord::Base
   has_many :return_operations, class_name: 'Operation::Return'
 
   has_one :slack_member
+
+  scope :not_have_slack_member, lambda {
+    User.left_joins(:slack_member).where(slack_members: { id: nil })
+  }
+
+  def self.notify_slack_member_not_bind
+    notify_slack("#{not_have_slack_member.pluck(:email).join(',')} は、slackのユーザとの紐付けができていないようです。このページにアクセスして紐付けを実施してください。：#{Rails.application.config.application_domain}/#{Rails.application.routes.url_helpers.slack_members_path}")
+  end
 
   def self.find_for_google(auth)
     user = User.find_by(email: auth.info.email)
