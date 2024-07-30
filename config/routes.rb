b14de5294071222e65834373013a5b5f43df8ee6
yukimura1227 <takamura1227@gmail.com>
2024-07-30 23:34:11 +0900

    revert routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  resources :slack_members, only: %i(index edit update destroy)
+  resources :books, only: %i(index edit update) do
+    member do
+      post :rental
+      post :want_to_read
+      post :return
+    end
+  end
+  resources :orders do
+    member do
+      post :purchase
+    end
+    collection do
+      get :extract_amazon_product_info
+    end
+  end
+  root to: 'top#index'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_scope :user do
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
